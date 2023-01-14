7451276cc3675365802c1f8bee864b26cdee14b8
Alex <alexmankiev@gmail.com>
2023-01-14 20:02:53 +0200

    Два числа фор.

---
+++ service/src/main/java/ua/edson/twomath/Application.java
@@ -0,0 +1,13 @@
+package ua.edson.twomath;
+
+public class Application {
+    public static void main(String[] args) {
+
+        // Написать цикл for, идеально сделать его бесконечным.
+        // В теле цикла вводить 2 числа и математический цикл.
+        // Высчитывать выражение.
+
+
+
+    }
+}
